862ae93962f41ee25facdb78929d4d8841a318dd
shcodenick <wojtek.kloc@gmail.com>
2023-07-05 19:23:18 +0200

    ecs stuff

--- main.tf
+++ main.tf
@@ -479,3 +479,32 @@ resource "aws_ecs_cluster" "ecs_cluster" {
   }
 }
 
+resource "aws_iam_role" "ecs_task_exec_role" {
+  name               = "${var.PRE}ecs-task-exec-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+  tags = {
+    Name = "${var.PRE}ecs-task-exec-role"
+    Owner = var.OWNER
+  }
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = aws_iam_role.ecs_task_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "ecsECRAccess_policy" {
+  role       = aws_iam_role.ecs_task_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"
+}
